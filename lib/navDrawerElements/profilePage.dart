11f61a55a99cc5b666eeff104230d5a758bb22ee
Faruque Aziz <faruqueaziz611@gmail.com>
2020-12-08 23:42:19 +0530

    rollout done

--- lib/navDrawerElements/profilePage.dart
+++ lib/navDrawerElements/profilePage.dart
@@ -1,5 +1,5 @@
 import 'package:SafaiSathi/notifier/authNotifier.dart';
-import 'package:SafaiSathi/services.dart';
+//import 'package:SafaiSathi/services.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
